f5b79c30696a681f775c8b741f45ab882a541f98
young <1160415122@qq.com>
2017-09-04 11:00:33 +0800

    【新增】添加了newToast的位置改变

--- app/src/main/java/com/young/toastshow/MainActivity.java
+++ app/src/main/java/com/young/toastshow/MainActivity.java
@@ -2,6 +2,7 @@ package com.young.toastshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,14 +16,18 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_long).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NewToastShow.makeText(MainActivity.this, "test", NewToastShow.TOAST_LONG).show();
+                NewToastShow newToastShow = NewToastShow.makeText(MainActivity.this, "test", "this is a test", NewToastShow.TOAST_LONG);
+                newToastShow.setGravity(Gravity.TOP, 0, 20);
+                newToastShow.show();
             }
         });
 
         findViewById(R.id.btn_short).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NewToastShow.makeText(MainActivity.this, "test", NewToastShow.TOAST_LONG).show();
+                NewToastShow newToastShow = NewToastShow.makeText(MainActivity.this, "test", "this is a test", NewToastShow.TOAST_SHORT);
+                newToastShow.setGravity(Gravity.TOP, 0, 20);
+                newToastShow.show();
             }
         });
     }
--- app/src/main/java/com/young/toastshow/NewToastShow.java
+++ app/src/main/java/com/young/toastshow/NewToastShow.java
@@ -30,8 +30,15 @@ public class NewToastShow {
     public static final double TOAST_LONG = 3;
     public static final double TOAST_SHORT = 1.5;
 
+    private double time;
+    private static Handler handler;
+    private Timer showTimer;
+    private Timer cancelTimer;
+
+    private Toast toast;
+
     private int width;
-    private View shadowBar, showbar;
+    private View shadowBar;
 
     private void setView(Context context, String text, String message) {
         LayoutInflater inflater = LayoutInflater.from(context);
@@ -39,7 +46,6 @@ public class NewToastShow {
         TextView title = (TextView) view.findViewById(R.id.title);
         TextView msg = (TextView) view.findViewById(R.id.message);
         shadowBar = view.findViewById(R.id.shadow_bar);
-        showbar = view.findViewById(R.id.show_bar);
 
         title.setText(text);
         msg.setText(message);
@@ -55,13 +61,8 @@ public class NewToastShow {
         layoutParams1.setMargins(margin, 0, 0, 0);
         shadowBar.setLayoutParams(layoutParams1);
 
-        RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams(width-margin*2, 3);
-        layoutParams2.setMargins(margin, 0, 0, 0);
-        showbar.setLayoutParams(layoutParams2);
 
         width -= margin*2;
-
-//        showbar.setLayoutParams(layoutParams);
 
         if (toast != null) {
             toast.cancel();
@@ -70,36 +71,6 @@ public class NewToastShow {
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setView(view);
     }
-
-//
-//    public void show(){
-//
-//        ValueAnimator va ;
-//        va = ValueAnimator.ofInt(width, 200);
-//        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-//                //获取当前的height值
-//                int w =(Integer)valueAnimator.getAnimatedValue();
-//                Log.e("show", w+"");
-//                //动态更新view的高度
-//                showbar.getLayoutParams().width = w;
-//                showbar.requestLayout();
-//            }
-//        });
-//        va.setDuration(lengthShort);
-//        //开始动画
-//        va.start();
-//
-//        toast.show();
-//    }
-
-    private double time;
-    private static Handler handler;
-    private Timer showTimer;
-    private Timer cancelTimer;
-
-    private Toast toast;
 
     private NewToastShow(Context context, String text, String message){
         showTimer = new Timer();
@@ -111,12 +82,18 @@ public class NewToastShow {
         this.time = time;
     }
 
+
     public static NewToastShow makeText(Context context, String text, String message, double time){
-        NewToastShow toast1= new NewToastShow(context, text, message);
-        toast1.setTime(time);
+        NewToastShow newToastShow= new NewToastShow(context, text, message);
+        newToastShow.setTime(time);
         handler = new Handler(context.getMainLooper());
-        return toast1;
+        return newToastShow;
     }
+
+    public void setGravity(int gravity, int xOffset, int yOffset) {
+        toast.setGravity(gravity, xOffset, yOffset);
+    }
+
 
     public void show(){
         showTimer.schedule(new TimerTask() {
@@ -144,15 +121,15 @@ public class NewToastShow {
     private class ShowRunnable implements Runnable{
         @Override
         public void run() {
-            final ValueAnimator va = ValueAnimator.ofInt(width, 0);
+            final ValueAnimator va = ValueAnimator.ofInt(0, width);
             va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator valueAnimator) {
                     //width
                     int w = (Integer) valueAnimator.getAnimatedValue();
                     //动态更新view的高度
-                    showbar.getLayoutParams().width = w;
-                    showbar.requestLayout();
+                    shadowBar.getLayoutParams().width = w;
+                    shadowBar.requestLayout();
 
                 }
             });
